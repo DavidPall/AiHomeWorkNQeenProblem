0b45d38f5cbf0a346f556faa2b7a2d8dd41e33ba
bojthe <55790397+bojthe@users.noreply.github.com>
2020-04-12 13:42:47 +0300

    Initial placement

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import random
+import numpy as np
+
+def print_board(board):
+    for row in board:
+        print(row)
+
 print("Szia Robiii. :D")
+
+n = int(input("Number of queens: "))
+
+initial_placement = list(range(n))
+random.shuffle(initial_placement)
+
+board = np.zeros((n,n), dtype=int)
+for i in range(n):
+    board[i][initial_placement[i]] = 1
+
+print_board(board)
