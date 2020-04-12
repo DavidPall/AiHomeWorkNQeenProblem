7f76c924e6652ede813940049a6abcbd760fecc7
David Pall <davidpallatx@gmail.com>
2020-04-12 14:54:38 +0300

    check diagonals of the matrix

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import random
 import numpy as np
 
+
 def print_board(board):
     for row in board:
         print(row)
 
-print("Szia Robiii. :D")
+
+def check_diag(board):
+    mdiag = np.zeros(len(board)*2 - 1, dtype=int)
+    sdiag = np.zeros(len(board)*2 - 1, dtype=int)
+
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == 1:
+                sdiag[i+j] += 1
+                mdiag[len(board) - i + j - 1] += 1
+
+    col_sum = 0
+    for i in range(len(board) * 2 - 1):
+        if mdiag[i] > 1:
+            col_sum += 1
+        if sdiag[i] > 1:
+            col_sum += 1
+
+    return mdiag, sdiag, col_sum
+
 
 n = int(input("Number of queens: "))
 
@@ -16,4 +36,4 @@ board = np.zeros((n,n), dtype=int)
 for i in range(n):
     board[i][initial_placement[i]] = 1
 
-print_board(board)
+check_diag(board)
